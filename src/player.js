312037f4c2940c31a32f7faf3da412a6c98fa97e
Caylin Vu <caylinvu@gmail.com>
2023-06-20 13:57:34 -0500

    Add attack and random attack functions to player class

--- src/player.js
+++ src/player.js
@@ -4,12 +4,27 @@ class Player {
         this.hits = [];
     }
 
-    attack() {
-
+    attack(coord, gameboard) {
+        if (gameboard.includesArray(this.hits, coord)) {
+            return;
+        }
+        gameboard.receiveAttack(coord);
+        this.hits.push(coord);
     }
 
-    randomAttack() {
-        
+    randomCoord() {
+        const row = Math.floor(Math.random() * 10);
+        const col = Math.floor(Math.random() * 10);
+        return [row, col];
+    }
+
+    randomAttack(gameboard) {
+        let randomCoord = this.randomCoord();
+        while (gameboard.includesArray(this.hits, randomCoord)) {
+            randomCoord = this.randomCoord();
+        }
+        gameboard.receiveAttack(randomCoord);
+        this.hits.push(randomCoord);
     }
 }
 
--- tests/player.test.js
+++ tests/player.test.js
@@ -10,7 +10,7 @@ test('player attacks', () => {
     const enemyGameboard = new Gameboard();
     const testPlayer = new Player('player');
     enemyGameboard.placeShip(5, [[3,2],[4,2],[5,2],[6,2],[7,2]]);
-    testPlayer.attack([3, 2]);
+    testPlayer.attack([3, 2], enemyGameboard);
     expect(enemyGameboard.ships[0].hitCount).toBe(1);
 });
 
@@ -20,7 +20,7 @@ test('randomly attacks', () => {
     enemyGameboard.placeShip(5, [[3,2],[4,2],[5,2],[6,2],[7,2]]);
     enemyGameboard.placeShip(4, [[1,5],[1,6],[1,7],[1,8]]);
     for (let i = 0; i < 100; i++) {
-        testPlayer.randomAttack();
+        testPlayer.randomAttack(enemyGameboard);
     }
     expect(enemyGameboard.allSunkStatus).toBe(true);
 });
