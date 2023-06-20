5d078a8fc3bfe1a57f1d54632c518b30dec68de4
Caylin Vu <caylinvu@gmail.com>
2023-06-20 13:34:00 -0500

    Set up player tests

---
+++ src/player.js
@@ -0,0 +1,16 @@
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.hits = [];
+    }
+
+    attack() {
+
+    }
+
+    randomAttack() {
+        
+    }
+}
+
+export default Player
---
+++ tests/player.test.js
@@ -0,0 +1,26 @@
+import Player from "../src/player";
+import Gameboard from "../src/gameboard";
+
+test('constructor creates a player', () => {
+    const testPlayer = new Player('player');
+    expect(testPlayer).toEqual({name: 'player', hits: []});
+});
+
+test('player attacks', () => {
+    const enemyGameboard = new Gameboard();
+    const testPlayer = new Player('player');
+    enemyGameboard.placeShip(5, [[3,2],[4,2],[5,2],[6,2],[7,2]]);
+    testPlayer.attack([3, 2]);
+    expect(enemyGameboard.ships[0].hitCount).toBe(1);
+});
+
+test('randomly attacks', () => {
+    const enemyGameboard = new Gameboard();
+    const testPlayer = new Player('player');
+    enemyGameboard.placeShip(5, [[3,2],[4,2],[5,2],[6,2],[7,2]]);
+    enemyGameboard.placeShip(4, [[1,5],[1,6],[1,7],[1,8]]);
+    for (let i = 0; i < 100; i++) {
+        testPlayer.randomAttack();
+    }
+    expect(enemyGameboard.allSunkStatus).toBe(true);
+});
